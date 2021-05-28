53ead61cb62867824c8ed80c414861d9a25a74a7
hayoungjeon <hayoungjeon@tamu.edu>
2021-05-27 21:54:12 -0500

    finding the longest substring without repeating characters

---
+++ longestSubstring.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int lengthOfLongestSubstring(char *s) {
+  if (*s == 0)
+    return 0;
+
+  int max = 0;
+
+  for (int i = 0; s[i]; i++) {
+    for (int j = 0; s[j]; j++) {
+      int duplicate = 0;
+      for (int k = i; k < j; k++) {
+        if (s[k] == s[j]) { // duplicates
+          duplicate = 1;
+          break;
+        }
+      }
+      if (duplicate == 1)
+        break;
+      for (int k = i; k <= j; k++) {
+        printf("%c", s[k]);
+      }
+      int length = j - i + 1;
+      if (length > max) {
+        max = length;
+      }
+      if (i <= j)
+        printf(", %d\n", length);
+    }
+  }
+
+  return max;
+}
+
+int main() {
+  int output;
+  output = lengthOfLongestSubstring("abcabcbb");
+
+  printf("%d\n", output);
+  return 0;
+}
