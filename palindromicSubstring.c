4de2e0872702847ce725fc576433aaf2eb0876f4
hayoungjeon <hayoungjeon@tamu.edu>
2021-05-30 23:58:52 -0500

    added description and test cases

--- palindromicSubstring.c
+++ palindromicSubstring.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
-char *longestPalindrome(char *s) { return s; }
+char *longestPalindrome(char *s) {
+  /*
+      Constraints: 1 <= s.length <= 1000
+      s consist of only digits and English letters (lower case and/or upper
+     case)
+  */
+  return s;
+}
 
-int main() { return 0; }
+int main() {
+  // test cases
+  char *s1 = "babad";
+  char *s2 = "cbbd";
+  char *s3 = "a";
+  char *s4 = "ac";
+  return 0;
+}
