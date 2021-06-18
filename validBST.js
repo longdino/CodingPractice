1413a8871ecebfa14eef55c4a1984abd8eb814a6
hayoungjeon <hayoungjeon@tamu.edu>
2021-06-17 23:48:06 -0500

    re-implemented the recursive function

--- validBST.js
+++ validBST.js
@@ -11,25 +11,11 @@
  * @return {boolean}
  */
 
-var _isValid = function (key, root) {
+var _isValid = function(key, root) {
     /*console.log("========================");
     console.log("key = " + key);
     console.log("root: " + root.val);*/
-    if (root.right) {
-        //console.log("right: " + root.right.val);
-        if (root.right.val <= root.val) return false;
-        if (root.val < key && root.right.val >= key) {
-            return false;
-        }
-        else {
-            if (!_isValid(root.val, root.right)) return false;
-        }
-        if (!_isValid(key, root.right)) {
-            return false;
-        }
-    }
-    /*console.log("========================");
-    console.log("root: " + root.val);*/
+    
     if (root.left) {
         //console.log("left: " + root.left.val);
         if (root.left.val >= root.val) return false;
@@ -37,16 +23,48 @@ var _isValid = function (key, root) {
             return false;
         }
         else {
-            if (!_isValid(root.val, root.left)) return false;
+            if(!_isValid(root.val, root.left)) return false;
         }
         if (!_isValid(key, root.left)) {
             return false;
         }
     }
-
+    if (root.right) {  
+        //console.log("right: " + root.right.val);
+        if (root.right.val <= root.val) return false;
+        if (root.val < key && root.right.val >= key) {
+            return false;
+        }
+        else {
+            if(!_isValid(root.val, root.right)) return false;
+        }
+        if (!_isValid(key, root.right)) {
+            return false;
+        }
+    }
+    /*console.log("========================");
+    console.log("root: " + root.val);*/
+    
     return true;
 };
 
-var isValidBST = function (root) {
-    return _isValid(root.val, root);
+var isValidBST = function(root) {
+    
+    if (!root) return true;
+    if (root.left) {
+        if (root.left.val >= root.val) return false;
+    }
+    if (root.right) {
+        if (root.right.val <= root.val) return false;
+    }
+    var left = isValidBST(root.left);
+    var right = isValidBST(root.right);
+    if (left && root.left.val.left) {
+        left = isValidBST(root.left.left);
+    }
+    if (right && root.right.right) {
+        right = isValidBST(root.right.right);
+    }
+    return left && right;
+    // return _isValid(root.val, root);
 };
