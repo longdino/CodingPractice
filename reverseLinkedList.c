76dda5a7ba2cca688c94538d1e0f8c2daca68ce5
hayoungjeon <hayoungjeon@tamu.edu>
2021-07-08 21:48:53 -0500

    reverse the linked list in recursive method

--- reverseLinkedList.c
+++ reverseLinkedList.c
@@ -6,18 +6,29 @@
  * };
  */
 
+// Recursive method
+
+struct ListNode* reverseList(struct ListNode* head) {
+    if(!head || !(head->next)) return head;
+    struct ListNode* rest = reverseList(head->next);
+    head->next->next = head;
+    head->next = NULL;
+    return rest;
+}
+
+// Iterative method
+/*
 struct ListNode* reverseList(struct ListNode* head){
     struct ListNode* curr = head;
     struct ListNode* prev = NULL;
     struct ListNode* next = NULL;
     
     while(curr) {
-        next = curr->next;
+        next = curr->next; 
         curr->next = prev;
         prev = curr;
         curr = next;
     }
-    
-    head = prev;
-    return head;
+    return prev;
 }
+*/
