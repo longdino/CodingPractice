bc32074d0e5a60212babd8c8f10a3f53b3651c89
hayoungjeon <hayoungjeon@tamu.edu>
2021-07-16 10:24:18 -0500

    flatten a multi-level doubly linked list working

--- flattenLinkedList.cpp
+++ flattenLinkedList.cpp
@@ -16,11 +16,10 @@ public:
         Node* tnext;
         Node* tprev;
         Node* ttail;
+        
         while(head) {
-            printf("value: %d\n", head->val);
-            
             if(head->child) {
-                tnext = head->next;
+                tnext = head->next ? head->next : NULL;
                 tprev = head->next ? head->next->prev: NULL;
                 lvl = flatten(head->child);
                 if(!lvl) break;
@@ -30,14 +29,20 @@ public:
                     ttail = ttail->next;
                 }
                 ttail->next = tnext;
-                tnext->prev = ttail;
+                if(tnext) tnext->prev = ttail;
                 head->next = lvl;
+                head->child = NULL;
+                if(ttail->next) head = ttail->next;
+                continue;
             }
-            
+            if(!head->next) {
+                while(head->prev) {
+                    head = head->prev;
+                }
+                return head;
+            }
             head = head->next;
         }
-        
         return head;
-       
     }
 };
